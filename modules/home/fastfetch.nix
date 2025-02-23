ada66a006ae6fcee54de56ef90e7b718f2c3651e
Arco Nightshade <paradoxzss07@raptorbytes.org>
2025-02-23 01:11:49 -0500

    Update fastfetch.nix

--- modules/home/fastfetch.nix
+++ modules/home/fastfetch.nix
@@ -6,7 +6,7 @@
     {
       "$schema": "https://github.com/fastfetch-cli/fastfetch/raw/dev/doc/json_schema.json",
       "logo": {
-        "source": "${~/nixos-config/Assets/HexOSlogo-REMOVEDBG.png}",
+        "source": "${~/HexOS/Assets/HexOSlogo-REMOVEDBG.png}",
         "type": "kitty-direct",
         "width": 33,
         "padding": {
